4a37ec7b25c089cc9e051cb7e923c70913675dee
Junha Kwon <junharoket@naver.com>
2025-05-15 17:43:26 +0900

    Update
    chap04 > Homework2 업데이트

--- chap04/src/pkg_homework/Homework2.java
+++ chap04/src/pkg_homework/Homework2.java
@@ -11,6 +11,11 @@ public class Homework2 {
 		System.out.print("기온 : ");
 		int temp = sc.nextInt();
 		
+		
+		// 에러나 예외 case 를 먼저 검사하고 처리한 다음, 성공 case 는 마지막에 처리하는 구조 = Early Return
+		// 장점: 코드의 가독성이 좋아짐, 코드의 중첩을 줄일 수 있음
+		// 이 경우 if(month < 1 || month > 12) 이런식으로 해야함
+		
 		if (month == 3 || month == 4 || month == 5) System.out.println("봄");
 		else if (month == 6 || month == 7 || month == 8) {
 			if (temp >= 33) {
