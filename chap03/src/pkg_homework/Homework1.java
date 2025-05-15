f21baebf4979f2441dd699475df64d37300d9cd6
Junha Kwon <junharoket@naver.com>
2025-05-15 17:17:28 +0900

    Update
    study > ex > MainClass 추가
    study > ex > RunEx 추가
    chap03 > Homework1 업데이트

--- chap03/src/pkg_homework/Homework1.java
+++ chap03/src/pkg_homework/Homework1.java
@@ -9,7 +9,8 @@ public class Homework1 {
 		System.out.print("말해보세요 : ");
 		String word = sc.next();
 		
-		String check = word.equals("간다") ? "온다" : "간다";
+		// String check = word.equals("간다") ? "온다" : "간다";
+		String check = "간다".equals(word) ? "온다" : "간다"; // 더 나은 코드(Null 이 들어갈 수도 있잖아, 한번 생각해봐)
 		System.out.println("앵무새 : " + check);
 		
 		sc.close();
