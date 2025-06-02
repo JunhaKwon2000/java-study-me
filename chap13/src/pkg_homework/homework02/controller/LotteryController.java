c0a9c530bf1231af99a8d1ccb75477cb70d4d242
Junha Kwon <junharoket@naver.com>
2025-06-02 17:31:53 +0900

    Update
    chap13 > homework02 update

--- chap13/src/pkg_homework/homework02/controller/LotteryController.java
+++ chap13/src/pkg_homework/homework02/controller/LotteryController.java
@@ -33,7 +33,7 @@ public class LotteryController {
 	}
 	
 	public Set<Lottery> winObject(){
-		if (lottery.size() >= 4) {
+		if (lottery.size() >= 4 && win.size() == 0) {
 			List<Lottery> temp = new ArrayList<>(lottery);
 			Set<Integer> random = new HashSet<>();
 			while(true) {
@@ -46,6 +46,23 @@ public class LotteryController {
 			}
 			return win;
 		}
+		else if (lottery.size() >= 4 && win.size() >= 1 && win.size() < 4) {
+			List<Lottery> temp = new ArrayList<>(lottery);
+			List<Lottery> temp2 = new ArrayList<>(win);
+			Set<Integer> random = new HashSet<>();
+			int check = 4 - win.size();
+			while(true) {
+				int i = (int)(Math.random() * temp.size());
+				if (temp2.indexOf(temp.get(i)) != -1) continue;
+				random.add(i);
+				if (random.size() == check) break;
+			}
+			for (int idx : random) {
+				win.add(temp.get(idx));
+			}
+			return win;
+		}
+		else if (win.size() == 4) return win;
 		else return null;
 	}
 	
