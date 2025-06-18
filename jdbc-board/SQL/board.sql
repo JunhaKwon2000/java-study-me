e1e8aec9f3ca872e4024c3d1c145969947fad906
Junha Kwon <junharoket@naver.com>
2025-06-18 10:58:27 +0900

    Update
    sql board update

---
+++ jdbc-board/SQL/board.sql
@@ -0,0 +1,37 @@
+-- 게시판 프로젝트 데이터베이스 생성
+CREATE DATABASE jdbc_board;
+USE jdbc_board;
+
+-- 게시판 테이블 생성
+CREATE TABLE board(
+	no INT AUTO_INCREMENT, 									-- 게시글 번호(PK)
+    title VARCHAR(100) NOT NULL, 							-- 게시글 제목(반드시 입력)
+	writer VARCHAR(100) NOT NULL, 							-- 게시글 작성자(반드시 입력)
+    content TEXT, 											-- 게시글 본문
+    reg_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 	-- 게시글 작성일(시간 포함, 반드시 입력, 기본값 = 지금)
+    -- reg_date DATE NOT NULL DEFAULT CURRENT_DATE
+    upd_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    -- 게시글 수정일(시간 포함, 반드시 입력, 기본값 = 지금, 업데이트 시 기본값으로 지금 시간 업데이트)
+    -- ON UPDATE CURRENT_TIMESTAMP: UPDATE 시 자동으로 수정 시간도 갱신
+    PRIMARY KEY(no)
+);
+
+DESC board;
+
+-- CRUD 테스트
+-- 추가 (게시글 번호, 작성일, 수정일은 안넣어도 됨)
+INSERT INTO board (title, writer, content) VALUES ('첫 게시글', '권준하', '게시글 본문');
+
+-- 조회
+SELECT * FROM board;
+SELECT * FROM board WHERE no = 1;
+
+-- 수정
+UPDATE board SET content = '수정된 게시글 본문' WHERE no = 1;
+-- 만약 테이블 만들 때 upd_date 컬럼에서 ON UPDATE CURRENT_TIMESTAMP 가 없다면
+-- upd_date = now() => 수정일을 현재 시간으로 갱신
+-- UPDATE board SET title = '수정된 게시글 제목', content = '수정된 게시글 본문', upd_date = now() WHERE no = 1;
+SELECT * FROM board WHERE no = 1;
+
+-- 삭제
+DELETE FROM board WHERE no = 1;
